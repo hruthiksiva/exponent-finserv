eba2acfb998e86f970a1e96278326a18e1b9b350
hruthiksiva <teram.hruthiksivakumar@gmail.com>
2024-12-08 00:23:05 +0530

    Contact Form Mobile Number added

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -9,6 +9,10 @@ const ContactForm = () => {
             <label htmlFor="floatingInput">Email address</label>
           </div>
           <div className="form-floating mb-3">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Mobile Number</label>
+          </div>
+          <div className="form-floating mb-3">
             <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
             <label htmlFor="floatingPassword">Password</label>
           </div>
