0bf74e64cec4c0c3ddc06392b3a9e04aeb391695
hruthiksiva <teram.hruthiksivakumar@gmail.com>
2024-12-07 23:19:29 +0530

    working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,24 @@
-
-
 const Hero = () => {
   return (
     <div>
-      <div className="container col-xxl-8 px-4 py-5">
+      <div className="container col-xxl-12 px-4 py-5">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div className="col-10 col-sm-8 col-lg-6">
-        <img src="src/assets/images/hero-1.jpg" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="1000" height="600" loading="lazy"/>
+      <div className="col-10 col-sm-8 col-lg-6" >
+           <img src="src/assets/images/hero-1.jpg" className="d-block mx-lg-auto img-fluid rounded-4 shadow" alt="Bootstrap Themes" width="600" loading="lazy" />
+          {/* <div style={{position:'absolute',right:'0px', width:'300px', height:'300px'}} className="rounded-circle bg-dark">‎</div> */}
       </div>
-      <div className="col-lg-6 text-justify">
-        <h1 className="display-5 fw-bold lh-1 mb-3">Expert Loaning Agent For Your Bussiness Success</h1>
-        <p className="lead">One platform for all your problems.</p>
+      <div className="col-lg-6">
+         {/* <p className=" fw-normal rounded-2">Every client growing 5X</p> */}
+         <div className="badge fw-normal bg-hero py-2 px-3 rounded-pill mb-2" style={{backgroundColor: '#c8dcf4', color: '#002147'}}>
+              Every client growing 5X&nbsp;<span>↗</span>
+        </div>
+        <h1 className="display-3 fw-bold lh-2 mb-3">Expert Loaning Agent For Your Bussiness Success</h1>
+        <p className="fw-normal mb-4">One platform for all your problems</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Contact Us</button>
+        <button type="button" className="btn btn-md px-4 py-2 me-md-2 custom-button" style={{backgroundColor: '#002147', borderColor: '#001a33',color: '#F4F0EE'}}>
+            Contact Us
+        </button>
+
         </div>
       </div>
     </div>
--- src/components/Legacy.jsx
+++ src/components/Legacy.jsx
@@ -1,26 +1,28 @@
-
 const Legacy = () => {
   return (
     <div>
-      <div className="container col-xxl-8 px-4 py-5">
-    <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div className="col-10 col-sm-8 col-lg-6">
-      <h1 className="display-5 fw-bold lh-1 mb-3">Our legacy of trust and precision</h1>
-        <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+      <div className="container col-xxl-12 px-auto py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <h1 className="display-6 fw-bold lh-2 mb-3">Responsive left-aligned hero with image</h1>
+            <p className="fw-normal mb-4">
+              Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
+            </p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" className="btn btn-md px-4 py-2 me-md-2 custom-button"  style={{backgroundColor: '#002147', borderColor: '#001a33',color: '#F4F0EE'}}>
+                Primary
+              </button>
+              <button type="button" className="btn btn-md px-4 py-2 me-md-2 custom-button"style={{backgroundColor: '#002147', borderColor: '#001a33',color: '#F4F0EE'}}>
+                Default
+              </button>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <img src="src/assets/images/hero-1.jpg" className="d-block mx-lg-auto img-fluid rounded-4 shadow" alt="Bootstrap Themes" width="600"  loading="lazy"/>
+          </div>
         </div>
       </div>
-      <div className="col-lg-6">
-      <img src="src/assets/images/legacy-1.jpg" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
-
-        
-      </div>
     </div>
-  </div>
-    </div>
-    
   )
 }
 
