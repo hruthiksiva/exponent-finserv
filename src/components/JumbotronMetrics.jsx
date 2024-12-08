cc412615f55f10ea8fa9bacebc4fc43a0f551e40
hruthiksiva <teram.hruthiksivakumar@gmail.com>
2024-12-08 17:40:21 +0530

    Jumbrotron changes

--- src/components/JumbotronMetrics.jsx
+++ src/components/JumbotronMetrics.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const JumbotronMetrics = () => {
   return (
     <div>
-        <div class="jumbotron jumbotron-fluid p-5">
-  <div class="container">
-    <h1 class="display-5 fw-bold">100 Cumulative Years Of Working Experience</h1>
-    <p class="lead">with 50+ Partners Across the Globe</p>
+        <div className="jumbotron jumbotron-fluid p-5">
+  <div className="container">
+    <h1 className="display-5 fw-bold">100 Cumulative Years Of Working Experience</h1>
+    <p className="lead">with 50+ Partners Across the Globe</p>
   </div>
 </div>
     </div>
