3d7076b47b18cd3759ed73e71d00179f1399069e
Manoj-Mukundan <manojmukunda4@gmail.com>
2024-12-08 20:03:51 +0530

    JumbotronMetrics and BankPartners has been added to the homepage

--- src/components/BankPartnership.jsx
+++ src/components/BankPartnership.jsx
@@ -1,7 +1,30 @@
-
+import bank from '../assets/images/banks/brand-1-1.png'
 const BankPartnership = () => {
+  const ImgIndex = Array.from({length:46},( _,i)=>i+1);
   return (
-    <div>BankPartnership</div>
+    
+    <div className="container p-5">
+      <div className="row">
+      {ImgIndex.map((index) => (
+        <div className="p-2 col-lg-1-5" key={index}>
+          <div className="card border-0 shadow-sm">
+            <div className="card-body">
+              <img
+                src={bank}
+                // src={`../assets/images/banks/brand-1-${index}.png`}
+                alt={`Bank ${index}`}
+                className='img-fluid'
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
+
+            </div>
+          </div>
+      </div>
+        ))}
+      </div>
+    </div>
+    
+    
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,8 @@ import Hero from '../components/Hero'
 import Legacy from '../components/Legacy'
 import MetricsCard from '../components/MetricsCard'
 import ServiceDetails from '../components/ServiceDetails'
-
+import BankPartners from '../components/BankPartnership'
+import JumbotronMetrics from '../components/JumbotronMetrics'
 
 
 const HomePage = () => {
@@ -14,6 +15,8 @@ const HomePage = () => {
       <MetricsCard />
       <br></br>
       <ServiceDetails isHome={true} />
+      <JumbotronMetrics/>
+      <BankPartners/>
       {/* <PartnerBanks /> */}
       <div className='container xl p-5'>
       <ContactCard />
