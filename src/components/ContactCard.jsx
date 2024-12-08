e0bdad2733fa9a9b6ef32cc37ebdd4526f880de7
Manoj-Mukundan <manojmukunda4@gmail.com>
2024-12-08 23:08:45 +0530

    ContactCards has been created

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,20 +1,18 @@
 const ContactCard = () => {
   return (
     <div>
-      <div className="card text-center">
-  <div className="card-header">
-    Featured
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-  <div className="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
+      <div className="container-xl py-5 text-center">
+      <div className="row">
+        <div className="col-md-12 d-flex align-text-center justify-content-center rounded-4" style={{backgroundColor:'#001247',width:'100%',height:'350px'}}>
+            <div className="lh-lg" style={{paddingTop:'80px',color:'#F4F0EE'}}>
+              <h1>Ready to transform your finances?</h1>
+              <p className="py-3 px-5 mx-5" style={{opacity:'60%'}}>Ultrices id sollicitudin amet cras tristique mattis adipiscing. Aliquet tempor nibh nunc <br/>vitae tincidunt pulvinar non fermentum.</p>
+              <a href="#" className="btn contact-us ">Contact Us</a>
+            </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
