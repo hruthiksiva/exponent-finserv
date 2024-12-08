3e620d324c405f573057f247d87ea0b29f2cf809
hruthiksiva <teram.hruthiksivakumar@gmail.com>
2024-12-08 17:29:16 +0530

    ServiceDetails updated

--- src/components/ServiceDetails.jsx
+++ src/components/ServiceDetails.jsx
@@ -1,31 +1,36 @@
-import ServiceCard from "./ServiceCard";
+import PropTypes from 'prop-types'; // Import PropTypes
+import ServiceCard from './ServiceCard';
 import servicesJ from '../serviceJson.json';
 
-const ServiceDetails = ({isHome=false}) => {
-   const services = isHome?servicesJ.slice(0,3):servicesJ;
+const ServiceDetails = ({ isHome = false }) => {
+  const services = isHome ? servicesJ.slice(0, 3) : servicesJ;
   return (
     <div>
       <div className="container-xl py-5">
         <div className="row px-5">
           <div className="col-10">
-          <h1 className="display-5 fw-bold lh-1 mb-3">Exponent Services</h1>
-          <p className="fs-5 lightGrey">We make the loan process seamless and easy by aggregating you on the right banks, so that you can make your dreams a reality.</p>
+            <h1 className="display-5 fw-bold lh-1 mb-3">Exponent Services</h1>
+            <p className="fs-5 lightGrey">We make the loan process seamless and easy by aggregating you on the right banks, so that you can make your dreams a reality.</p>
           </div>
           <div className="col-2 d-flex align-items-center justify-content-end">
             <button className="btn custom_button fw-bold py-2 px-3">All Services</button>
           </div>
         </div>
         <div className="row px-5 pt-4 pb-5">
-        {services.map((service)=> (
-            <div className="col-4">
-            <ServiceCard title={service.title}/>
+          {services.map((service) => (
+            <div key={service.title} className="col-4">
+              <ServiceCard title={service.title} />
             </div>
-            ))}
-            </div>
+          ))}
+        </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
+
+// Add PropTypes validation
+ServiceDetails.propTypes = {
+  isHome: PropTypes.bool, // 'isHome' is expected to be a boolean
+};
 
 export default ServiceDetails;
