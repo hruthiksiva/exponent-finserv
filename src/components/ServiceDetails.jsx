88989130fc394fb5a4c1d36dde03a0bd7308a3f7
Manoj-Mukundan <manojmukunda4@gmail.com>
2024-12-08 16:12:01 +0530

    Modified the ServiceDetails.jsx code to add service cards in homepage as well as in Service page

--- src/components/ServiceDetails.jsx
+++ src/components/ServiceDetails.jsx
@@ -1,6 +1,8 @@
-import ServiceCard from "./ServiceCard"
+import ServiceCard from "./ServiceCard";
+import servicesJ from '../serviceJson.json';
 
-const ServiceDetails = () => {
+const ServiceDetails = ({isHome=false}) => {
+   const services = isHome?servicesJ.slice(0,3):servicesJ;
   return (
     <div>
       <div className="container-xl py-5">
@@ -13,19 +15,13 @@ const ServiceDetails = () => {
             <button className="btn custom_button fw-bold py-2 px-3">All Services</button>
           </div>
         </div>
-        
-          <div className="row px-5 pt-4 pb-5">
+        <div className="row px-5 pt-4 pb-5">
+        {services.map((service)=> (
             <div className="col-4">
-            <ServiceCard title="Personal Loan" />
+            <ServiceCard title={service.title}/>
             </div>
-            <div className="col-4">
-            <ServiceCard title="Bussiness Loan"/>
+            ))}
             </div>
-            <div className="col-4">
-            <ServiceCard title="Education Loan"/>
-            </div>
-          
-        </div>
       </div>
     </div>
     
