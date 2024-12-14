8937876eae27531ca0e9e6c150d6ffb28a7b825e
Manoj-Mukundan <manojmukunda4@gmail.com>
2024-12-14 13:46:22 +0530

    Background image has been added to the ContactCard

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,23 +1,30 @@
 const ContactCard = () => {
   return (
     <div>
-      <div className="container-fluid col-11 px-5 py-5 text-center my-5">
-        <div className="row">
-          <div
-            className="col-12 d-flex align-items-center justify-content-center rounded-4 "
-            style={{ backgroundColor: '#002147', width: '100%', height: '350px' }}
-          >
-            <div className="lh-lg" style={{ paddingTop: '40px', color: '#F4F0EE', width: '90%' }}>
-              <h1 className="text-wrap">Ready to transform your finances?</h1>
-              <p className="py-3 px-4 mx-2" style={{ opacity: '60%' }}>
-                Are you ready to step-up? Contact us now to see how Exponent can elevate your financial journey.
-              </p>
-              <a href="#" className="btn contact-us">Contact Us</a>
-            </div>
+    <div className="container-fluid col-10 py-5 text-center">
+      <div className="row mx-auto">
+        <div
+          className="d-flex align-items-center justify-content-center rounded-4"
+          style={{
+            backgroundImage: 'url(/public/custom_card.png)', // replace with your image URL
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '100%',
+            height: '350px',
+          }}
+        >
+          <div className="lh-lg" style={{color: '#F4F0EE', width: '100%' }}>
+            <h1 className="text-wrap">Ready to transform your finances?</h1>
+            <p className="col-6 mx-auto" style={{ opacity: '85%' }}>
+              Are you ready to step-up? Contact us now to see how Exponent can elevate your financial journey.
+            </p>
+            <a href="#" className="btn contact-us">Contact Us</a>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  
   );
 };
 
